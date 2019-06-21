9e7ec25e0d90e65c76db329c238bfac0209274bf
Hitoshi Yoshida <github@peria.jp>
2019-06-21 20:44:10 +0900

    Implement Maps parser

--- src/map.cpp
+++ src/map.cpp
@@ -1,11 +1,78 @@
 #include "map.h"
 
-#include <string>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
 #include <sstream>
+#include <string>
 #include <vector>
 
-using Position = std::pair<int, int>;
+using Point = std::pair<int, int>;
+using Booster = std::pair<char, Point>;
 
-Map::Map(const std::string& desc) {
-  // Parse |desc| and construct a 2D map.
+namespace {
+
+Point ParsePoint(char*& p) {
+  assert (*p == '(');
+  Point pos;
+  int x = std::strtol(p, &p, 10);
+  assert (*p == ',');
+  int y = std::strtol(p, &p, 10);
+  assert (*p == ')');
+  ++p;
+
+  return {x, y};
+}
+
+std::vector<Point> ParseMap(char*& p) {
+  std::vector<Point> map_pos;
+
+  assert (*p == '(');
+  map_pos.emplace_back(ParsePoint(p));
+  while (*p == ',') {
+    map_pos.emplace_back(ParsePoint(++p));
+  }
+
+  return map_pos;
+}
+
+std::vector<std::vector<Point>> ParseObstacles(char*& p) {
+  std::vector<std::vector<Point>> obstacles;
+  assert (*p == '(');
+  obstacles.emplace_back(ParseMap(p));
+  while (*p == ';') {
+    obstacles.emplace_back(ParseMap(++p));
+  }
+  return obstacles;
+}
+
+Booster ParseBooster(char*& p) {
+  assert (std::strchr("BFLX", *p) != nullptr);
+  char code = *p;
+  Point point { ParsePoint(++p) };
+  return {code, point};
+}
+
+std::vector<Booster> ParseBoosters(char*& p) {
+  assert (std::strchr("BFLX", *p) != nullptr);
+  std::vector<Booster> boosters;
+  boosters.emplace_back(ParseBooster(p));
+  while (*p == ';') {
+    boosters.emplace_back(ParseBooster(++p));
+  }
+  return boosters;
+}
+
+}  // namespace
+
+Map::Map(const std::string& task) {
+  char* p = const_cast<char*>(task.data());
+
+  std::vector<Point> map_pos { ParseMap(p) };
+  assert (*p == '#');
+  Point start { ParsePoint(++p) };
+  assert (*p == '#');
+  std::vector<std::vector<Point>> obstacles { ParseObstacles(++p) };
+  assert (*p == '#');
+  std::vector<Booster> { ParseBoosters(++p) };
 }
