0ec3fb2fad96c229afc834f84dac4cd210446f1e
Hitoshi Yoshida <github@peria.jp>
2019-06-21 20:54:10 +0900

    Fix parse error

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include "map.h"
 
+#include <iostream>
+#include <string>
+
 int main(int argc, char* argv[]) {
+  std::string input;
+  std::getline(std::cin, input);
+  Map mp(input);
+
   return 0;
 }
