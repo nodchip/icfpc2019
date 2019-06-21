17a0cb1f599071420e838a048603d3655d386cd2
Takahiro SUZUKI <takahiro.suzuki.1986@gmail.com>
2019-06-21 09:33:42 -0700

    Makefile: clean before build

--- continuous_integration.sh
+++ continuous_integration.sh
@@ -3,6 +3,7 @@
 git submodule init
 git submodule update
 cd ${WORKSPACE}/src
+make clean || exit 1
 make -j ${NUMBER_OF_PROCESSORS} all || exit 1
 ./test --gtest_output=xml:test_result.xml || exit 1
 
