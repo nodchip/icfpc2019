63ac7a7873e010ffad1630b0a561e7ddd8f5ff87
Takahiro SUZUKI <takahiro.suzuki.1986@gmail.com>
2019-06-22 09:19:13 -0700

    add test for cloning

--- src/tests/test_actions.cpp
+++ src/tests/test_actions.cpp
@@ -48,3 +48,29 @@ TEST(ActionTest, AffectsOnGame) {
   wrapper->useBooster(Action::DRILL); game.tick();
   EXPECT_EQ(0, game.num_boosters[BoosterType::DRILL]);
 }
+
+TEST(ActionTest, Clone) {
+  // example-01.desc + mod.
+  Game game("(0,0),(10,0),(10,10),(0,10)#(0,0)#(4,2),(6,2),(6,7),(4,7);(5,8),(6,8),(6,9),(5,9)#C(0,1);X(0,2)");
+
+  Wrapper* wrapper = game.wrappers[0].get();
+  //
+  wrapper->move(Action::UP);
+  game.tick();
+  EXPECT_EQ(1, game.num_boosters[BoosterType::CLONING]);
+  //
+  wrapper->move(Action::UP);
+  game.tick();
+  //
+  Wrapper* wrapper_cloned = wrapper->cloneWrapper();
+  EXPECT_EQ(1, game.wrappers.size()); // not added yet.
+  game.tick();
+  EXPECT_EQ(2, game.wrappers.size()); // added.
+  EXPECT_EQ(wrapper_cloned, game.wrappers[1].get());
+  //
+  wrapper->move(Action::UP);
+  wrapper_cloned->move(Action::RIGHT);
+  game.tick();
+  EXPECT_EQ(Point(0, 3), wrapper->pos);
+  EXPECT_EQ(Point(1, 2), wrapper_cloned->pos);
+}
