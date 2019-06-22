6553b7000606b02c261b8fcd2aea7bd8a0e49d86
Takahiro SUZUKI <takahiro.suzuki.1986@gmail.com>
2019-06-21 18:56:15 -0700

    fix test case

--- src/tests/test_actions.cpp
+++ src/tests/test_actions.cpp
@@ -10,6 +10,7 @@ TEST(actions, Game_test_actions) {
   // example-01.desc
   Game game("(0,0),(10,0),(10,10),(0,10)#(0,0)#(4,2),(6,2),(6,7),(4,7);(5,8),(6,8),(6,9),(5,9)#B(0,1);B(1,1);F(0,2);F(1,2);L(0,3);X(0,9)");
 
+  constexpr int P = CellType::kWrappedBit;
   constexpr int W = CellType::kObstacleBit;
   constexpr int B = CellType::kBoosterManipulatorBit;
   constexpr int F = CellType::kBoosterFastWheelBit;
@@ -17,8 +18,8 @@ TEST(actions, Game_test_actions) {
   constexpr int R = CellType::kBoosterTeleportBit;
   constexpr int X = CellType::kBoosterUnknownXBit;
   Map2D ground_truth(10, 10, {
-    1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-    B, B, 0, 0, 0, 0, 0, 0, 0, 0,
+    P, P, 0, 0, 0, 0, 0, 0, 0, 0,
+    B, B|P, 0, 0, 0, 0, 0, 0, 0, 0,
     F, F, 0, 0, W, W, 0, 0, 0, 0,
     L, 0, 0, 0, W, W, 0, 0, 0, 0,
     0, 0, 0, 0, W, W, 0, 0, 0, 0,
