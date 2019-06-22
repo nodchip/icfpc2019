382e467c39601699a73130cf4f3e7f0c24158a56
Hisayori Noda <nodchip@gmail.com>
2019-06-22 18:29:55 +0900

    Added a script to execute engines.

---
+++ scripts/execute_engine.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+rm -Rf solutions/${ENGINE_NAME}
+./scripts/python/execute_engines.py \
+--description_directory_path dataset/problems \
+--solution_directory_path solutions/${ENGINE_NAME} \
+--best_solution_directory_path best_solutions \
+--engine_file_path src/solver \
+--jobs `grep processor /proc/cpuinfo | wc -l` \
+--solver_name ${ENGINE_NAME} || exit 1
+
+./scripts/python/compare_engine.py \
+--solution_base_directory_path solutions \
+--engine_name_file_paths engine_names.txt \
+--output_html_file_path compare_engine.html || exit 1
