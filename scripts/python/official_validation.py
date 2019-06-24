dfab17b98a62c201b8122bd5e33545ef3aff4ac5
Takahiro SUZUKI <takahiro.suzuki.1986@gmail.com>
2019-06-24 02:42:59 -0700

    support buy

--- scripts/python/official_validation.py
+++ scripts/python/official_validation.py
@@ -31,13 +31,18 @@ class Validator(object):
     def __del__(self):
         self.driver.quit()
 
-    def validate(self, desc_path, sol_path):
+    def validate(self, desc_path, sol_path, buy_path=None):
 
         desc_file = self.driver.find_element_by_id('submit_task')
         desc_file.send_keys(os.path.abspath(desc_path))
 
         sol_file = self.driver.find_element_by_id('submit_solution')
         sol_file.send_keys(os.path.abspath(sol_path))
+
+        if buy_path is not None and len(open(buy_path).read()) > 0:
+            print('use buy file')
+            buy_file = self.driver.find_element_by_id('submit_boosters')
+            buy_file.send_keys(os.path.abspath(buy_path))
 
         button = self.driver.find_element_by_id('execute_solution')
         button.click()
@@ -69,23 +74,25 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--desc-dir')
     parser.add_argument('--sol-dir')
+    parser.add_argument('--buy-dir')
     args = parser.parse_args()
 
     pairs = []
     for desc_path in glob.glob(os.path.join(args.desc_dir, '*.desc')):
         sol_path = os.path.join(args.sol_dir, os.path.splitext(os.path.basename(desc_path))[0] + '.sol')
-        pairs.append((desc_path, sol_path))
+        buy_path = os.path.join(args.buy_dir, os.path.splitext(os.path.basename(desc_path))[0] + '.buy')
+        pairs.append((desc_path, sol_path, buy_path))
 
     v = Validator()
     results = []
-    for desc_path, sol_path in tqdm.tqdm(pairs):
+    for desc_path, sol_path, buy_path in tqdm.tqdm(pairs):
         if not os.path.isfile(sol_path):
             res = {
                 'succeeded': False,
                 'message': 'file not exist',
             }
         else:
-            res = v.validate(desc_path, sol_path)
+            res = v.validate(desc_path, sol_path, buy_path)
         
         problem_index = -1
         mo = re.match(r'prob-(\d+)\.desc', os.path.basename(desc_path))
@@ -97,6 +104,7 @@ def main():
             'sol_path': sol_path,
             'result': res,
         })
+        print(results[-1])
     
     del v
     print(json.dumps(results, indent=4))
