4dc6d8317c7ab3b7b8edf70338c3951e3ea8f89f
Takahiro SUZUKI <takahiro.suzuki.1986@gmail.com>
2019-06-24 01:25:45 -0700

    semimanual works

--- src/solvers/semimanual.cpp
+++ src/solvers/semimanual.cpp
@@ -26,6 +26,8 @@ struct Plan {
   Game* game;
   std::vector<std::queue<CommandBase*>> commands;
   std::map<int, Point> cover_centers;
+  std::function<void()> init_round_functor;
+  std::function<void(int, Wrapper*)> free_move_functor;
   bool debug = false;
 
   Plan(Game* g) : game(g) {}
@@ -48,6 +50,7 @@ struct Plan {
     while (!game->isEnd()) {
       if (debug) std::cout << "===== " << game->time << std::endl;
       if (debug) std::cout << *game << std::endl;
+      if (init_round_functor) init_round_functor();
 
       bool all_terminatable = true;
       for (int i = 0; i < terminatable.size(); ++i) {
@@ -68,7 +71,7 @@ struct Plan {
         // continue or get new command.
         if (!processing_command[i]) {
           if (commands[i].empty()) {
-            w->nop();
+            freeMove(i, w);
             continue;
           } else {
             if (debug) std::cerr << "pop next command" << std::endl;
@@ -103,7 +106,7 @@ struct Plan {
           if (debug) std::cerr << "t=" << game->time << " i=" << i << " pos=" << w->pos << ": pick C " << c->pos << std::endl;
           if (w->pos == c->pos) {
             // reached.
-            w->nop(); // MUDA!
+            freeMove(i, w);
             processing_command[i] = nullptr; // finished.
           } else {
             // not reached, move.
@@ -113,12 +116,13 @@ struct Plan {
           }
         } else if (SetCoverCenter* c = dynamic_cast<SetCoverCenter*>(cmd)) {
           if (debug) std::cerr << "t=" << game->time << " i=" << i << " pos=" << w->pos << ": set cover " << c->pos << std::endl;
+          cover_centers[i] = c->pos;
           auto targets = findNearestPoints(enumerateCellsByMask(game->map2d, CellType::kObstacleBit, 0), c->pos);
           assert (!targets.empty());
           auto target = targets[0];
           if (w->pos == target) {
             // reached.
-            w->nop(); // MUDA!
+            freeMove(i, w);
             processing_command[i] = nullptr; // finished.
             terminatable[i] = true;
           } else {
@@ -134,6 +138,14 @@ struct Plan {
       game->tick();
       displayAndWait(param, game);
       if (iter_callback && !iter_callback(game)) return;
+    }
+  }
+private:
+  void freeMove(int i, Wrapper* w) {
+    if (free_move_functor) {
+      free_move_functor(i, w);
+    } else {
+      w->nop();
     }
   }
 };
@@ -154,14 +166,14 @@ std::map<int, ManualFunction> manual_specs = {
     plan(0, new PickC{{80, 116}});
     plan(0, new Spawn{{135, 149}});
     plan(0, new PickC{{152, 171}});
-    plan(1, new Spawn{{132, 57}});
+    plan(1, new Spawn{{143, 69}});
     plan(1, new PickC{{122, 113}});
     plan(2, new Spawn{{123, 39}});
 
-    plan(0, new SetCoverCenter{{200, 200}});
-    plan(1, new SetCoverCenter{{0, 200}});
-    plan(2, new SetCoverCenter{{200, 0}});
-    plan(3, new SetCoverCenter{{0, 0}});
+    plan(0, new SetCoverCenter{{150, 150}});
+    plan(1, new SetCoverCenter{{0, 150}});
+    plan(2, new SetCoverCenter{{150, 0}});
+    plan(3, new SetCoverCenter{{100, 100}});
     return plan;
   }},
 };
@@ -169,59 +181,69 @@ std::map<int, ManualFunction> manual_specs = {
 std::string semimanualSolver(SolverParam param, Game* game, SolverIterCallback iter_callback) {
   std::cerr << "Problem #" << game->problem_no << std::endl;
 
-  // if exists, execute semimanual plan
-  if (manual_specs.find(game->problem_no) != manual_specs.end()) {
-    std::cerr << "found semimanual plan" << std::endl;
-    Plan plan = manual_specs[game->problem_no](game);
-    plan.execute(param, iter_callback);
-  }
-
   // continue (or start)
   // do not care about further cloning.
   ConnectedComponentAssignmentForParanoid cc_assignment(game,
     10 /* distance_threshold */,
     100 /* small_region_bonus */);
   
-  std::vector<ManipulatorExtender> extenders;
-  for (auto& w : game->wrappers) {
-    extenders.emplace_back(game, w.get());
+  std::map<int, std::unique_ptr<ManipulatorExtender>> extenders;
+  for (int i = 0; i < game->wrappers.size(); ++i) {
+    extenders[i] = std::make_unique<ManipulatorExtender>(game, game->wrappers[i].get());
   }
 
+  auto free_move = [&](int i, Wrapper* w) {
+    auto pos = w->pos;
+
+    // manipulator
+    if (game->num_boosters[BoosterType::MANIPULATOR] > 0) {
+      if (extenders.find(i) == extenders.end()) {
+        extenders[i] = std::make_unique<ManipulatorExtender>(game, w);
+      }
+      extenders[i]->extend();
+      return;
+    }
+
+    // paranoid
+    std::vector<Trajectory> trajs;
+    cc_assignment.update();
+    if (cc_assignment.hasDisjointComponents() && cc_assignment.isComponentAssignedToWrapper(w->index)) {
+      Trajectory t;
+      if (pointToDirection(t.last_move, cc_assignment.getSuggestedMotionOfWrapper(w->index))) {
+        trajs = { t };
+      }
+    }
+
+    // BFS
+    if (trajs.empty()) {
+      trajs = map_parse::findNearestUnwrapped(*game, pos, DISTANCE_INF);
+    }
+
+    if (trajs.empty()) {
+      w->nop();
+    } else {
+      w->move(Direction2Char(trajs[0].last_move));
+    }
+  };
+
+  if (manual_specs.find(game->problem_no) != manual_specs.end()) {
+    // if exists, execute semimanual plan
+    std::cerr << "found semimanual plan" << std::endl;
+    Plan plan = manual_specs[game->problem_no](game);
+    plan.init_round_functor = [&] {
+      cc_assignment.delayUpdate();
+    };
+    plan.free_move_functor = free_move;
+    plan.execute(param, iter_callback);
+  }
+
+  // otherwise, some poor solver.
   while (!game->isEnd()) {
     cc_assignment.delayUpdate();
 
     for (int i = 0; i < game->wrappers.size(); ++i) {
-      auto& w = game->wrappers[i];
-      auto pos = w->pos;
-
-      // manipulator
-      if (game->num_boosters[BoosterType::MANIPULATOR]) {
-        extenders[i].extend();
-        continue;
-      }
-
-      // paranoid
-      std::vector<Trajectory> trajs;
-      cc_assignment.update();
-      if (cc_assignment.hasDisjointComponents() && cc_assignment.isComponentAssignedToWrapper(w->index)) {
-        Trajectory t;
-        if (pointToDirection(t.last_move, cc_assignment.getSuggestedMotionOfWrapper(w->index))) {
-          trajs = { t };
-        }
-      }
-
-      // BFS
-      if (trajs.empty()) {
-        trajs = map_parse::findNearestUnwrapped(*game, pos, DISTANCE_INF);
-      }
-
-      if (trajs.empty()) {
-        w->nop();
-      } else {
-        w->move(Direction2Char(trajs[0].last_move));
-      }
+      free_move(i, game->wrappers[i].get());
     }
-
     game->tick();
     displayAndWait(param, game);
     if (iter_callback && !iter_callback(game)) return game->getCommand();
